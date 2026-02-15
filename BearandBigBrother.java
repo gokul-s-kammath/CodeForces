f23d86df94e55dfc5a0895e1f1ad9aafbc095384
gokul-s-kammath <gokul.s.1551@gmail.com>
2026-02-15 20:19:02 +0530

    Create  BearandBigBrother.java

---
+++ BearandBigBrother.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int year=0;
+        while(a<=b)
+        {
+            a=a*3;
+            b=b*2;
+            year++;
+        }
+        System.out.println(year);
+       
+    }
+}
